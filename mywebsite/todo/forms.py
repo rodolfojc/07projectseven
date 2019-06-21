b632cc87fe9016a74c30d1dda9f3fa3e1044c05b
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-21 22:01:52 +0100

    Views, Urls and Index.html
    Finished

--- mywebsite/todo/forms.py
+++ mywebsite/todo/forms.py
@@ -4,7 +4,7 @@ class TodoForm(forms.Form):
     text = forms.CharField(max_length=50,
     widget = forms.TextInput(
     attrs={
-        'class': 'form-control'
-        'placeholder' : 'A Django TODO'
+        'class': 'form-control',
+        'placeholder': 'A Django TODO'
     }
     ))
--- mywebsite/todo/urls.py
+++ mywebsite/todo/urls.py
@@ -6,4 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('add', views.addNewTodo, name = 'add'),
+    path('complete/<todo_id>', views.completeTodo, name = 'complete'),
+    path('delete', views.deleteTodo, name = 'delete'),
 ]
